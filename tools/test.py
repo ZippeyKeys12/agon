98ed9ecbcd9de875fcf79d2d41d71e055d10815f
ZippeyKeys12 <ZippeyKeys12@gmail.com>
2018-09-22 23:53:29 -0500

    Special treatment
    Add config and specifier file
    Need to move to some text database for data

--- tools/test.py
+++ tools/test.py
@@ -2,6 +2,6 @@
 import os
 from subprocess import run
 
-run(["py", "tools/bobby/build.py", "Agon", "-C", "-V3.3"])
+run(["py", "tools/bobby/build.py", "Agon", "-C", "-V3.3", "-IZCONFIG.ini"])
 print("Running Steam")
 run(["C:\\Program Files (x86)\\Steam\\Steam.exe", "-applaunch", "2300"])
