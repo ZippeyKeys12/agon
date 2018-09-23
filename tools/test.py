2992ad4bc17c1cf3b6e363838672fe94c5e1decb
ZippeyKeys12 <ZippeyKeys12@gmail.com>
2018-09-22 22:28:40 -0500

    Uses bobby now

--- tools/test.py
+++ tools/test.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python3
 import os
 from subprocess import run
 
-run(["py", "tools/build/build.py"])
+run(["py", "tools/bobby/build.py", "Agon", "-C", "-V3.3"])
 print("Running Steam")
 run(["C:\\Program Files (x86)\\Steam\\Steam.exe", "-applaunch", "2300"])
